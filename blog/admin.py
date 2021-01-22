64bea153a890ac468c19fa60e554b31e994cc49d
Hussein <husas1998@gmail.com>
2021-01-22 17:03:21 +0330

    remove old stuff

--- blog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Blog
-
-
-class BlogAdmin(admin.ModelAdmin):
-    list_display = ('title', 'date_time')
-
-
-admin.site.register(Blog, BlogAdmin)
